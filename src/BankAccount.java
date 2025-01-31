274f5f73574c8827ceeaf0a9368882fda2d89003
miguel-the <miguel.alonzo02@utrgv.edu>
2025-01-31 18:33:47 +0000

    : )

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,19 @@
 
+public class BankAccount 
+{
+    public static void main(String[] args)
+    {
+        double total = 2175.37;
+
+        total -= 302.50;
+        total += 50.03;
+        total /= 2;
+        total += 20.00;
+        total -= 1;
+        total *= 2;
+        total += 1;
+
+
+        System.out.printf("%.2f", total);
+    }
+}
